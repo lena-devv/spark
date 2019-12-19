bee8c5017b04f39c23892355c5e2533903b6c67b
Elena_Nazarova <Elena_Nazarova@epam.com>
2019-12-19 19:23:02 +0300

    Fix scala code style violation errors

--- mllib/src/main/scala/org/apache/spark/ml/Pipeline.scala
+++ mllib/src/main/scala/org/apache/spark/ml/Pipeline.scala
@@ -21,9 +21,11 @@ import java.{util => ju}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
+
 import org.apache.hadoop.fs.Path
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
+
 import org.apache.spark.SparkContext
 import org.apache.spark.annotation.{DeveloperApi, Since}
 import org.apache.spark.internal.Logging
@@ -83,8 +85,8 @@ abstract class PipelineStage extends Params with Logging with
   override def copy(extra: ParamMap): PipelineStage
 
   override protected def doPostEvent(
-	listener: MLListener,
-	event: MLListenEvent): Unit = {
+  listener: MLListener,
+  event: MLListenEvent): Unit = {
     listener.onEvent(event)
   }
 }
